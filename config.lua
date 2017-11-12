6628ebe997c5400dc48f86c254d202d9855c1939
Pandorina <gregos1810@gmail.com>
2017-11-12 17:58:49 +0100

    Facto + action sur le point + menu répa

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 
 Config = {
 	DrawDistance = 100,
-	Price = 5000,
+	Price = 200,
 	BlipInfos = {
 		Sprite = 290,
 		Color = 38 
@@ -22,13 +22,15 @@ Config.Garages = {
 			Pos = {x=229.700, y= -800.1149, z= 29.5722},
 			Color = {r=0,g=255,b=0},
 			Size  = {x = 3.0, y = 3.0, z = 1.0},
-			Marker = 1
+			Heading=157.84,
+			Marker = 1		
 		},
 		DeletePoint = {
 			Pos = {x=215.124, y=-791.377, z=29.646},
 			Color = {r=255,g=0,b=0},
 			Size  = {x = 3.0, y = 3.0, z = 1.0},
-			Marker = 1
+			Marker = 1,
+			
 		},
 		MunicipalPoundPoint = {
 			Pos = {x=482.896, y=-1316.557, z=28.301},
@@ -40,7 +42,8 @@ Config.Garages = {
 			Pos = {x=490.942, y=-1313.067, z=27.964},
 			Color = {r=0,g=255,b=0},
 			Size  = {x = 3.0, y = 3.0, z = 1.0},
-			Marker = 1
+			Marker = 1,
+			Heading=299.42
 		},
 	},
 	Garage_Paleto = {	
